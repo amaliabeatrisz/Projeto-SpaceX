6c89f195f52552bc677c00cc81951392fb6aa931
amaliabeatrisz <amalia.beatrisz@gmail.com>
2022-10-20 01:48:28 -0300

    navbar mobile

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,3 @@
-// $('.header_right').click(function () {
-//     $(this).toggleClass('active');
-//     console.log("Clicked menu");
-//     $("#mobile").toggleClass("show_list");
-//     $("#mobile").fadeIn();
-
-// });
+$('#hamburger').click(function() {
+    $("#menu").slideToggle();
+  });
